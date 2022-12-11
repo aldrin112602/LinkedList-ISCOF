cafcd43848eb9525dbbeda49c423bf4b597212a4
Aldrin Caballero <76159577+aldrin112602@users.noreply.github.com>
2022-12-12 01:29:20 +0800

    Create activity 1.java

---
+++ activity 1.java
@@ -0,0 +1,6 @@
+public class Main {    
+    public static void main(String[] args) {
+        String[][] iscof = {{"C", "O", "F"}, {"I", "S"}};
+        System.out.println(iscof[1][0] + iscof[1][1] + iscof[0][0] + iscof[0][1] + iscof[0][2]);
+    }    
+}
